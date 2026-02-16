4c836cf8ac3a2cb7a886dcb362c2f8a59758344c
Janvi Vashistha <janvi.vashistha@gmail.com>
2026-02-16 23:25:39 +0530

    Coded UC2 to print OOPS in banner format

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,23 @@
 /**
  * OOPS Banner App
  *
- * UC1: Print OOPS to Console
+ * UC2: Print OOPS in Banner Format using * and spaces
  *
  * @author Janvi Vashistha
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****   ***** ");
+
     }
 }
+
