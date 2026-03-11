eadd5869ea11832b42556fbda7abf0ba41f11391
Janvi Vashistha <janvi.vashistha@gmail.com>
2026-03-11 08:54:45 +0530

    UC2: Display Hello using command line argument

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,8 +1,17 @@
+/**
+ * UC2: Display Hello with Command Line Argument
+ * @author Janvi Vashistha
+ */
+
 public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello User");
+        }
 
     }
 }
