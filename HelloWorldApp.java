dee9e3c4c4577f42a5056c83573e3ab6868d2f1c
Janvi Vashistha <janvi.vashistha@gmail.com>
2026-03-17 08:26:40 +0530

    Revert "UC2: Display Hello using command line argument"
    This reverts commit eadd5869ea11832b42556fbda7abf0ba41f11391.

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,17 +1,8 @@
-/**
- * UC2: Display Hello with Command Line Argument
- * @author Janvi Vashistha
- */
-
 public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello User");
-        }
+        System.out.println("Hello World");
 
     }
 }
